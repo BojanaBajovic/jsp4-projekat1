c8ffdd2dd649fd938ff14a1897263e9621d441fc
Bojana Bajovic <bokicabajovic@yahoo.com>
2019-02-02 12:25:11 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
 function setup() {
   createCanvas(400, 300);
-  background(0,100,0);
+  background(200);
 
   strokeWeight(4);
 
   fill(0,170,0);
   rect(width/2-50, height/2-50, 100, 100);
-} 
 
-// function draw() {
-//   background(0,100,0);
-// }
+  fill(255);
+  ellipse(width/2-50+25, height/2-50+30, 20, 20)
+  ellipse(width/2-50+70, height/2-50+30, 30, 30)
+
+  stroke(0);
+  line(width/2-30, height/2+30, width/2+30, height/2+30)
+
+  noStroke();
+  rect(width/2-30, height/2+18, 10, 10);
+  rect(width/2+20, height/2+18, 10, 10);
+
+  stroke(0);
+  translate(width/2-60,height/2-60); 
+  fill(255,100,30);
+  triangle(0, 0, 0, 60, 60, 0);
+}
